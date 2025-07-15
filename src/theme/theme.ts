bfe29b5de1757655d400c98143f7bbf737c0aea2
Paddy <Paddygnewton@hotmail.co.uk>
2025-07-15 20:35:39 +0100

    added theme and colours

---
+++ src/theme/theme.ts
@@ -0,0 +1,9 @@
+export const colours = {
+    red: "#ef4444",
+    blue: "#3b82f6",
+    green: "#10b981",
+    orange: "#f97316",
+    gray: "#6b7280",
+    black: "#000000",
+    white: "#ffffff",
+  };
