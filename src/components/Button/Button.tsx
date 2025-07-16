510efb6e8d279abe69c83035034be42a43dfe2c7
Paddy <Paddygnewton@hotmail.co.uk>
2025-07-16 19:03:57 +0100

    Added colour prop to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,43 @@
 import React from "react";
 import clsx from "clsx";
+import { ColourName } from "../../theme";
+import { useTheme } from "../../theme/ThemeProvider";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: "default" | "outline" | "cancel";
+  colour?: ColourName;
 };
 
 export const Button: React.FC<ButtonProps> = ({
   variant = "default",
   className,
   children,
+  colour,
   ...props
 }) => {
+  const { colours } = useTheme();
+
   const base =
     "px-4 py-2 rounded font-medium transition-colors border border-radius-2px";
-  const variants = {
-    default: "bg-blue-600 text-white hover:bg-blue-700",
+  const variantClasses = {
+    default: `text-white`,
     outline: "border border-gray-300 text-gray-800 hover:bg-gray-100",
     cancel: "bg-red-600 text-white hover:bg-red-700",
   };
 
+  const bgColorStyle =
+    variant === "default" && colour
+      ? {
+          backgroundColor: colours[colour],
+        }
+      : undefined;
+
   return (
-    <button className={clsx(base, variants[variant], className)} {...props}>
+    <button
+      className={clsx(base, variantClasses[variant], className)}
+      style={bgColorStyle}
+      {...props}
+    >
       {children}
     </button>
   );
